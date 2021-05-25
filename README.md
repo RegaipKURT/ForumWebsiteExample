d273b2659f470080985df4f73d8e4888323da769
RegaipKURT <regkrt@gmail.com>
2021-05-25 22:29:41 +0300

    veritabanları ve kullanıcı login, logout, signup metodları oluşturuldu.

--- README.md
+++ README.md
@@ -1,1 +1,48 @@
 # ForumWebsiteExample
+
+## Açıklamalar
+Bu web sitesi örneği Python 3.8.10 ve Flask 2.0.1 versiyonu kullanılarak deneme amaçlı oluşturulmuştur.
+
+***Flask 2.0 versiyonundan itibaren değişmiş olan yeni routing metodları kullanıldığından 2.0 önceki versiyonlarda hata verecektir!***
+
+Veritabanı SQLite veritabanı kullanılarak oluşturulmuştur. Tek bir database dosyası içinde oluşturulan Users, Messages ve Posts tabloları kullanılarak bir forum sitesindeki en temel işlemler 
+yerine getirilmeye çalışılmıştır.
+
+### ***User*** tablosu için create ifadesi aşağıdaki gibidir:
+```sql
+CREATE TABLE "User" (
+	"id"	INTEGER NOT NULL UNIQUE,
+	"username"	TEXT NOT NULL UNIQUE,
+	"email"	TEXT NOT NULL UNIQUE,
+	"password"	TEXT NOT NULL,
+	"isModerator"	INTEGER NOT NULL,
+	PRIMARY KEY("id" AUTOINCREMENT)
+); 
+```
+
+
+### ***Post*** tablosu için create ifadesi:
+```sql
+CREATE TABLE "Post" (
+	"id"	INTEGER NOT NULL UNIQUE,
+	"ownerID"	INTEGER NOT NULL,
+	"header"	TEXT NOT NULL,
+	"body"	TEXT NOT NULL,
+	"openComments"	INTEGER NOT NULL,
+	"postDate"	TEXT NOT NULL,
+	PRIMARY KEY("id" AUTOINCREMENT)
+);
+```
+
+### Son olarak ***Message*** tablosu için oluşturma ifadesi:
+```sql
+CREATE TABLE "Message" (
+	"id"	INTEGER NOT NULL UNIQUE,
+	"ownerID"	INTEGER NOT NULL,
+	"postID"	INTEGER NOT NULL,
+	"message"	TEXT NOT NULL,
+	"messageDate"	TEXT NOT NULL,
+	PRIMARY KEY("id" AUTOINCREMENT)
+);
+```
+</code></pre>
--- main.py
+++ main.py
@@ -1,16 +1,20 @@
-from flask import Flask, render_template, url_for, redirect
+from flask import Flask, render_template, url_for, \
+    redirect, request
+from flask.helpers import send_from_directory
 from flask_sqlalchemy  import SQLAlchemy
 from flask_login import LoginManager, UserMixin, login_user, \
     login_required, logout_user, current_user
+from sqlalchemy.exc import IntegrityError
 from werkzeug.security import generate_password_hash, check_password_hash
 import shortuuid, uuid, sqlite3
+import re
 
 app = Flask(__name__)
 
 #Application Settings
 app.config['SECRET_KEY'] = "42c78b0e-5dbc-4083-8794-b745930a1b71"
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///database.db"
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
 app.config['TESTING'] = False
 app.config.update(
     SESSION_COOKIE_SECURE=True,
@@ -27,21 +31,21 @@ login_manager.init_app(app)
 login_manager.login_view = 'login'
 
 class User(UserMixin, db_users.Model):
-    userId = db_users.Column(db_users.Integer, primary_key=True)
-    userName = db_users.Column(db_users.String(15), unique=True, nullable=False)
-    eMail = db_users.Column(db_users.String(50), unique=True, nullable=False)
-    password = db_users.Column(db_users.String(150), nullable=False)
+    id = db_users.Column(db_users.Integer, primary_key=True)
+    userName = db_users.Column(db_users.String(500), unique=True, nullable=False)
+    eMail = db_users.Column(db_users.String(500), unique=True, nullable=False)
+    password = db_users.Column(db_users.String(500), nullable=False)
     isModerator = db_users.Column(db_users.Boolean, nullable=False)
 
 class Message(db_messages.Model):
-    messageId = db_messages.Column(db_messages.Integer, primary_key=True)
+    id = db_messages.Column(db_messages.Integer, primary_key=True)
     ownerId = db_messages.Column(db_messages.Integer, nullable=False)
     postId = db_messages.Column(db_messages.Integer, nullable=False)
     message = db_messages.Column(db_messages.String(500), nullable=False)
     date = db_messages.Column(db_messages.DateTime)
 
 class Post(db_posts.Model):
-    postId = db_posts.Column(db_posts.Integer, primary_key=True)
+    id = db_posts.Column(db_posts.Integer, primary_key=True)
     ownerId = db_posts.Column(db_posts.Integer)
     header = db_posts.Column(db_posts.String(100), default="Konu Yok")
     body = db_posts.Column(db_posts.String(5000), nullable=False)
@@ -50,7 +54,7 @@ class Post(db_posts.Model):
 
 @login_manager.user_loader
 def load_user(user_id):
-    return User.get(user_id)
+    return User.query.get(int(user_id))
 
 # app.errorhandler accepts error codes if you want to use 
 # your customized error pages for different type of errors.
@@ -70,7 +74,29 @@ def post(post_id):
 
 @app.get("/login")
 def login():
-    return render_template("login.html")
+    if current_user.is_authenticated:
+        return render_template("/profile.html")
+    else:
+        return render_template("login.html")
+
+
+@app.post('/login')
+def loginUser():
+    # try:
+        if request.method == "POST":
+            email = re.sub(' +', ' ', str(request.form['email']).strip())
+            password = str(request.form['password'])
+            user = User.query.filter_by(eMail=email).first()
+            if user:
+                if check_password_hash(user.password, password):
+                    login_user(user, remember=True)
+                    return render_template("index.html")
+                else:
+                    return render_template("login.html", warn="Yanlış Bir Kullanıcı Adı veya Şifre Girdiniz!")
+            else:
+                return render_template("login.html", warn="Yanlış Bir Kullanıcı Adı veya Şifre Girdiniz!")
+    # except:
+    #     return render_template("unknownError.html"), 500
 
 @app.get("/signup")
 def signup():
@@ -78,12 +104,40 @@ def signup():
 
 @app.post('/signup')
 def signupUser():
-    return render_template("login.html")
+    try:
+        if request.method == "POST":
+            email = re.sub(' +', ' ', str(request.form['email']).strip())
+            password = str(request.form['password'])
+            username = re.sub(' +', ' ', str(request.form['username']).strip())
+            isModerator = 0
+            
+            uyari = '''Lütfen Girdiğiniz Değerleri kontrol ediniz! \nŞifre en az 8 karakter olmalı ve diğer değerler de uygun şekilde girilmiş olmalıdır!'''
+            
+            if username == "" or username == " " or password == "" or password == " " or len(password) < 8:
+                return render_template("signup.html", warn=uyari)
 
-@app.post('/login')
-def loginUser():
+            hashed_password = generate_password_hash(password, method='pbkdf2:sha512:80000')
+            new_user = User(userName=username, eMail=email, password=hashed_password, isModerator=isModerator)
+            db_users.session.add(new_user)
+            db_users.session.commit()
+            
+            print(hashed_password, username, password, email)
+            return render_template("signup.html", warn="Kullanıcı eklendi.\nGiriş yapmak için giriş sayfasını kullanın.\n")
+    
+    except IntegrityError: #kullanıcı varsa dönecek hata
+            return render_template("signup.html", warn="Kullanıcı adı veya E-mail sistemde zaten kayıtlı!")
+    
+    #başka bir hata oluşması durumunda unkownError sayfasını gösteriyorum
+    #hataya göre alınacak aksiyonlar farklı olarak belirlenmelidir!
+    except:
+        return render_template("unknownError.html"), 500
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
     return render_template("index.html")
-
 #user's settings page
 @app.get("/settings")
 @login_required
@@ -97,8 +151,18 @@ def about():
 #user profile page defined with username
 @app.get("/profile/<string:username>")
 @login_required
-def profilePage():
-    return render_template("profile.html")
+def profilePage(username):
+    try:
+        return render_template(f"profile/{username}.html")
+    except:
+        return render_template("unknownError.html"), 500
+
+
+
+
+@app.get("/robots.txt")
+def robots():
+    return send_from_directory("static", path="/static", filename="robots.txt")
 
 if __name__=='__main__':
     app.run("0.0.0.0", port=8000)
